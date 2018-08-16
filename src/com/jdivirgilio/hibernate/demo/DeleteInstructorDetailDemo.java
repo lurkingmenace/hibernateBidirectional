c72d427c40cf4636c951cbd780f29091e6b828fd
John DiVirgilio <lurkingmenace@gmail.com>
2018-08-16 13:16:05 -0500

    Added delete example

---
+++ src/com/jdivirgilio/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,50 @@
+package com.jdivirgilio.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.jdivirgilio.hibernate.demo.entity.Instructor;
+import com.jdivirgilio.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		// Create Session Factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml") // Default name of file. Not
+																					// necessary to include here.
+																					// Must be in class path though!
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+
+
+		Session session = factory.getCurrentSession();
+		try {
+			session.beginTransaction();
+
+			// get the instructor detail object
+			int id = 2; // checked via MySQL workbench;
+			InstructorDetail detail = session.get(InstructorDetail.class, id);
+
+			// print the instructor detail
+			System.out.println("Instructor Detail: " + detail);
+
+			// print the associated instructor
+			System.out.println("Instructor: " + detail.getInstructor());
+			
+			// Delete it
+			session.delete(detail);
+
+			session.getTransaction().commit();
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+}
